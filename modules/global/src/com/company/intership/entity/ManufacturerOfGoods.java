dec21228dde904af2da3e15cbaee5c7ce8fc7818
Mikhail Shikhalev <shikhalev.mike@yandex.ru>
2024-07-10 17:42:24 +0400

    D1&UI1

--- modules/global/src/com/company/intership/entity/ManufacturerOfGoods.java
+++ modules/global/src/com/company/intership/entity/ManufacturerOfGoods.java
@@ -1,8 +1,50 @@
 package com.company.intership.entity;
 
+import com.haulmont.chile.core.annotations.NamePattern;
 import com.haulmont.cuba.core.entity.StandardEntity;
-import javax.persistence.Entity;
+import com.haulmont.cuba.security.entity.User;
 
-@javax.persistence.Entity
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Table(name = "INTERSHIP_MANUFACTURER_OF_GOODS")
+@Entity(name = "intership_ManufacturerOfGoods")
+@NamePattern("%s|name")
 public class ManufacturerOfGoods extends StandardEntity {
+    private static final long serialVersionUID = -5354441049094555650L;
+
+    @NotNull
+    @Column(name = "NAME", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "FULL_NAME")
+    private String fullName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
